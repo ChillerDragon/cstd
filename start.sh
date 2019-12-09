4e50f94476eb62a9685050dd48bbb10dd88cd5d2
ChillerDragon <ChillerDragon@gmail.com>
2019-12-09 14:44:18 +0100

    Add localhost dev mode for start script

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-./pstd_server.pl -l 149.202.127.134:8080 -H 149.202.127.134:8080
+#!/bin/bash
+if [ "$1" == "dev" ]
+then
+    ./pstd_server.pl -l localhost:8080 -H localhost:8080
+else
+    ./pstd_server.pl -l 149.202.127.134:8080 -H 149.202.127.134:8080
+fi
+
