39b289f67fa9f77efa5ed82ad3d8d85d6713effa
ChillerDragon <ChillerDragon@gmail.com>
2019-12-16 11:46:54 +0100

    Verbose in dev mode

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 mkdir -p pastes
 if [ "$1" == "dev" ]
 then
-    ./pstd_server.pl -l localhost:8080 -H localhost:8080
+    ./pstd_server.pl -l localhost:8080 -H localhost:8080 -v
 else
     ./pstd_server.pl -l 149.202.127.134:8080 -H 149.202.127.134:8080
 fi
