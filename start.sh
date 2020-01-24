9f95f372aa27f92d5db8fb7ecc91592fa1529587
ChillerDragon <ChillerDragon@gmail.com>
2020-01-24 09:30:40 +0100

    Use zillyhuhn.com domain

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ if [ "$1" == "dev" ]
 then
     ./pstd_server.pl -l localhost:8080 -H localhost:8080 -v
 else
-    ./pstd_server.pl -l 149.202.127.134:8080 -H 149.202.127.134:8080
+    ./pstd_server.pl -l 0.0.0.0:8080 -H zillyhuhn.com:8080
 fi
 
